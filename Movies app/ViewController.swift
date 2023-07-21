36e0acc919ac90d4c55e630b1e1a2909d35597da
ZhanabekZ <zhanabekzinekenov@gmail.com>
2023-07-21 22:39:53 +0600

    Initial Commit

---
+++ Movies app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies app
+//
+//  Created by ZhZinekenov on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
